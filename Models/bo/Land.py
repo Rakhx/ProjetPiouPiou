5a84fc7312943055f481cdc9df679b82f583019a
Rakhx <audren.bouadjio@gmail.com>
2023-02-21 14:46:35 +0100

    Update Land.py

--- Models/bo/Land.py
+++ Models/bo/Land.py
@@ -10,16 +10,20 @@ class Land():
         self._dimension = dimension
         self._items = items
         # initialise le plateau a vide
-        self._plateau = [["-" for x in range(int(dimension[0]))] for y in range(int(dimension[1]))]
+        self._plateau = {}
+        for itemz in items:
+            self._plateau[itemz.getPosition()] = itemz
 
     def getDimension(self) -> Tuple[float, float]:
         return self._dimension
 
     def addItem(self, item : Item ):
         self._items.append(item)
+        self._plateau[item.getPosition()] = item
 
     def removeItem(self, item : Item):
-        pass
+        self._items.remove(item)
+        self._plateau.remove(item)
 
     def isItemIsAtPos(self, position : Tuple[float, float]) -> bool:
         for item in self._items :
@@ -31,7 +35,14 @@ class Land():
 
     # Une unité regarde autour d'elle
     def lookAround(self, position):
-        pass
+        retour = {}
+        for x in range(-1,2):
+            for y in range(-1,2):
+                pos = (position[0]+x, position[1]+y)
+                if pos in self._plateau:
+                    retour[pos] = self._plateau[pos]
+                    
+        return retour
 
     # inverse le contenu du plateau pour pos1 et pos2
     def deplacerUnite(self, position1, position2):
